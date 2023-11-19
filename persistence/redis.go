40ebab15f6fa2a38c62c5e601e23dff49c06e8e0
robertjmcginness <code.rjmcginness@gmail.com>
2023-11-19 00:55:29 -0500

    11_19_2023: Init Redis Client

--- persistence/redis.go
+++ persistence/redis.go
@@ -4,12 +4,8 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var redis_client: redis
+var redis_client: *redis.Client
 
 func init() {
 	redis_client  = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
-}
-
-func connect() {
-	redis.
 }
